84b43426ef911be5f0d702238c9e300325b3e976
Reza Mohammadi <rezamohammadi8001@gmail.com>
2023-09-25 23:10:05 +0330

    complete splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:uniquegram/screens/login_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    goToNextPage(context);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          repeat: ImageRepeat.repeat,
+          image: AssetImage('assets/images/pattern.png'),
+        ),
+      ),
+      child: const Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 80.0),
+              child: Center(
+                child: Image(
+                  image: AssetImage('assets/images/logo.png'),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 30,
+              child: Column(
+                children: [
+                  Text(
+                    'Uniquegram',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 25,
+                    ),
+                  ),
+                  Text(
+                    'An Instagram but with Unique UI',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> goToNextPage(BuildContext context) async {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const LoginScreen(),
+        ),
+      ),
+    );
   }
 }
