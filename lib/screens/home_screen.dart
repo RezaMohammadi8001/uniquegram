4259d6568725ed490462da98016ec90a9ef98b9d
Reza Mohammadi <rezamohammadi8001@gmail.com>
2023-10-10 18:49:00 +0330

    fixed some ui bugs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,67 +11,67 @@ class HomeScrenn extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff1C1F2E),
-      body: CustomScrollView(
-        physics: const BouncingScrollPhysics(),
-        slivers: [
-          SliverAppBar(
-            pinned: true,
-            toolbarHeight: 65.w,
-            backgroundColor: const Color(0xff1C1F2E),
-            flexibleSpace: Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: 30.0.w,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 27.w),
-                    child: const Text(
-                      'Uniquegram',
-                      style: TextStyle(
-                        fontFamily: 'GB',
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xffF35383),
+      body: SafeArea(
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              toolbarHeight: 65.w,
+              backgroundColor: const Color(0xff1C1F2E),
+              flexibleSpace: Padding(
+                padding: EdgeInsets.only(top: 20.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 27.w),
+                      child: const Text(
+                        'Uniquegram',
+                        style: TextStyle(
+                          fontFamily: 'GB',
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xffF35383),
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(right: 27.w),
-                    child: SizedBox(
-                      width: 24.w,
-                      height: 24.w,
-                      child: Image.asset(
-                        'assets/images/icon_direct.png',
+                    Padding(
+                      padding: EdgeInsets.only(right: 27.w),
+                      child: SizedBox(
+                        width: 24.w,
+                        height: 24.w,
+                        child: Image.asset(
+                          'assets/images/icon_direct.png',
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          SliverToBoxAdapter(
-            child: SizedBox(
-              height: 100.w,
-              child: ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => index == 0
-                    ? Padding(
-                        padding: EdgeInsets.only(left: 17.0.w),
-                        child: const AddStoryWidget(),
-                      )
-                    : const StoryWidget(),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 100.w,
+                child: ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => index == 0
+                      ? Padding(
+                          padding: EdgeInsets.only(left: 17.0.w),
+                          child: const AddStoryWidget(),
+                        )
+                      : const StoryWidget(),
+                ),
               ),
             ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) => const PostWidget(),
-            ),
-          )
-        ],
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => const PostWidget(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
